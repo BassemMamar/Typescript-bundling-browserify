7b2fb4d8014a02b4b412470fbc2ad8575723b1ee
bassemmamar <bassemmamar@gmail.com>
2017-12-11 22:14:45 +0300

    include  async/await for es7 support

--- app/helper/helper.ts
+++ app/helper/helper.ts
@@ -3,24 +3,39 @@ var fs = require("fs");
 const src = fs.readFileSync('app/home.html', 'utf8');
 
 class helper {
- 
+
     constructor() {
-     
-    }    
+        this.delayedMessage("World", 4000).then(() => { console.log('done') });
+        this.delayedMessage("Hello", 2000);
+    }
 
     static sum(a: number, b: number) {
         var deferred = $.Deferred<void>();
 
-       setTimeout(function () {
+        setTimeout(function () {
             $('#container').html(src);
             $('#container').append('Done with sync.');
-       }, 1000);
+        }, 1000);
         return a + b;
     }
 
     promiseFunc(): JQueryPromise<boolean> {
         return null;
     }
+
+    sleep(duration: number) {
+        return new Promise(function (resolve) {
+            setTimeout(() => { resolve(0) }, duration);
+        })
+    }
+
+    async  delayedMessage(message: string, delay: number) {
+        let remainingTime = await this.sleep(delay);
+        console.log(message, `( ${message} remaining time: ${remainingTime})`);
+    }
+
+
+
 }
 
 
--- app/main.ts
+++ app/main.ts
@@ -5,15 +5,36 @@ const src = fs.readFileSync('app/home.html', 'utf8');
 const stl = fs.readFileSync('app/style.css', 'utf8');
 
 export class main {
+    divOut = '';
+    hlpr: helper;
     constructor(public firstNumber: number, public secondNumber: number) {
         console.log('test ', src);
         console.log('style content', stl);
+         this.start();
+        this.hlpr = new helper();
     }
-  
+
     getSum() {
         console.log('getSum Func');
         let result = helper.sum(this.firstNumber, this.secondNumber);
         return result;
+    }
+
+
+    sqrt(para: any): any {
+        return new Promise(resolve => setTimeout(_ => resolve(para * para), 1000));
+    }
+
+    async   start() {
+        console.log('start printing...');
+        for (var i = 0; i < 3; i++) {
+            var result = await this.sqrt(i);
+            console.log('<h3> i :' + i + ' </h3>');
+            for (var j = 0; j < result; j++) {
+                console.log(`<h5>          // j: ${j} ...  sqrt(${j}): ${await this.sqrt(j)} <h5>`);
+                console.log('');
+            }
+        }
     }
 
 }
